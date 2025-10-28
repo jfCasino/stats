4aaf469c5e6a7188672738b65d13f6fada849799
Jaka Furlan <jaka.furlan@cosylab.com>
2025-10-28 19:23:02 +0100

    initialised spring boot

---
+++ roulette-app/stats-service/src/test/java/com/jfCasino/stats_service/StatisticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfCasino.stats_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
