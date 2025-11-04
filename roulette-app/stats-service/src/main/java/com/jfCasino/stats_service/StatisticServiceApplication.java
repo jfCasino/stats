c3687dbeb8d1ded9f57144c881271af90e58e67a
Jaka Furlan <jaka.furlan@cosylab.com>
2025-11-04 20:46:05 +0100

    created parent pom, changed child poms

--- roulette-app/stats-service/src/main/java/com/jfCasino/stats_service/StatisticServiceApplication.java
+++ roulette-app/stats-service/src/main/java/com/jfCasino/stats_service/StatisticServiceApplication.java
@@ -2,8 +2,10 @@ package com.jfCasino.stats_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//TODO remove exclude when db is configured		
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StatisticServiceApplication {
 
 	public static void main(String[] args) {
